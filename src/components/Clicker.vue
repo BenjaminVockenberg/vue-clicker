18ea5cbf772c00aeb33f74997bf9081387a96c65
benjamin.vockenberg <benjamin.vockenberg@gmail.com>
2018-08-16 01:06:29 +0200

    changes some layout things

--- src/components/Clicker.vue
+++ src/components/Clicker.vue
@@ -11,18 +11,24 @@
                 <p><strong>Multiplicator:</strong> {{ multiplicator / scoreReducer }} {{ scoreReducer > 1 ? 'M' : ''}}</p>
             </div>
 
-            <div class="clickelement" @click="updateScore()">
+            <div class="clickelement" @click="updateScore()" :style="{ padding: paddingFactor + '%' }">
                 <img src="./../assets/facebookLogo.png" alt="" style="width: 100%">
             </div>
 
             <div class="actions">
-                <p>x2</p>
-                <div class="progress" :style="{ width: updateProgress(1) + '%'}">{{ updateProgress(1) }} %</div>
+                <p>Multiplicator x2</p>
+                <div class="progress_border">
+                    <div class="progress" :style="{ width: updateProgress(1) + '%'}"></div>
+                </div>
+                <p>{{ updateProgress(1) }} %</p>
                 <div class="btn">
                     <button v-show="(score >= 100 * multiplicator)" type="button" name="double" @click="buymulti(2)">x2</button>
                 </div>
-                <p>x4</p>
-                <div class="progress" :style="{ width: updateProgress(4) + '%'}">{{ updateProgress(4) }} %</div>
+                <p>Multiplicator x4</p>
+                <div class="progress_border">
+                    <div class="progress" :style="{ width: updateProgress(4) + '%'}"></div>
+                </div>
+                <p>{{ updateProgress(4) }} %</p>
                 <div class="btn">
                     <button v-show="score >= 400 * multiplicator" type="button" name="quad" @click="buymulti(4)">x4</button>
                 </div>
@@ -38,67 +44,83 @@
 
 <script>
 export default {
-  name: 'Clicker',
-  data() {
-      return {
-          score: 0,
-          highscore: 0,
-          scoreReducer: 1,
-          multiplicator: 1
-      }
-  },
-  methods : {
-      /**
-       * @name buymulti
-       * @param mulit {number}
-       * @desc buying a multiplicator x2
-       **/
-      buymulti(multi) {
-          this.score -= 100 * this.multiplicator;
-          this.multiplicator *= multi;
-          this.updateProgress(multi);
-      },
-      /**
-       * @name updateScore
-       * @desc updating the score call highscore
-       **/
-      updateScore() {
-          this.score += 1 * this.multiplicator;
-          this.updateHighScore();
-          this.cutScore();
-      },
-      /**
-       * @name updateHighScore
-       * @desc updating the highscore
-       **/
-      updateHighScore() {
-          if (this.score >= this.highscore) {
-              this.highscore = this.score;
-          }
-      },
-      /**
-       * @name updateProgress
-       * @param value {number}
-       * @return number
-       * @desc updateing the progress bar
-       **/
-      updateProgress(value) {
-          return Math.round((((this.score * 100) / 100) - ((100 * this.multiplicator * value) / 100 )) / this.multiplicator / value);
-      },
-      /**
-       * @name cutScore
-       * @desc divides score by 1m to make it more readable
-       **/
-      cutScore() {
-          if (this.score > 1000000) {
-              this.scoreReducer = 1000000;
-          }
-      }
-  }
+    name: 'Clicker',
+    data() {
+        return {
+            score: 0,
+            highscore: 0,
+            scoreReducer: 1,
+            paddingFactor: 0,
+            multiplicator: 1
+        }
+    },
+    methods : {
+        /**
+        * @name buymulti
+        * @param mulit {number}
+        * @desc buying a multiplicator x2
+        **/
+        buymulti(multi) {
+            this.score -= 100 * this.multiplicator;
+            if (this.score < 0) {
+                this.score = 0;
+            }
+            this.multiplicator *= multi;
+            this.updateProgress(multi);
+        },
+        /**
+        * @name updateScore
+        * @desc updating the score call highscore
+        **/
+        updateScore() {
+            this.score += 1 * this.multiplicator;
+            this.updateHighScore();
+            this.updatePadding();
+            this.cutScore();
+        },
+        /**
+        * @name updateHighScore
+        * @desc updating the highscore
+        **/
+        updateHighScore() {
+            if (this.score >= this.highscore) {
+                this.highscore = this.score;
+            }
+        },
+        /**
+        * @name updateProgress
+        * @param value {number}
+        * @return number
+        * @desc updateing the progress bar
+        **/
+        updateProgress(value) {
+            return Math.round((((this.score * 100) / 100) - ((100 * this.multiplicator * value) / 100 )) / this.multiplicator / value);
+        },
+        /**
+        * @name cutScore
+        * @desc divides score by 1m to make it more readable
+        **/
+        cutScore() {
+            if (this.score > 1000000) {
+                this.scoreReducer = 1000000;
+            }
+        },
+        /**
+        * @name updatePadding
+        * @desc giving a visual feedback of clicking the clicker clickelement
+        **/
+        updatePadding() {
+            this.paddingFactor += 1;
+            if (this.paddingFactor >= 1) {
+                setTimeout(() => {
+                    this.paddingFactor = 0;
+                }, 800);
+            }
+        }
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 * {
     padding: 0;
@@ -130,7 +152,8 @@ export default {
         width: 500px;
         height: 500px;
         float: left;
-        background-color: red;
+        padding: 0%;
+        background-color: orange;
     }
 
     .actions {
@@ -140,14 +163,19 @@ export default {
         height: 500px;
         padding: 25px;
 
-        .progress {
-            width: 0%;
-            max-width: 100%;
-            height: 20px;
-            background-color: green;
-            color: white;
-            line-height:0;
+        .progress_border {
+            width: 100%;
+            background-color: rgb(80,80,80);
+            .progress {
+                width: 0%;
+                max-width: 100%;
+                height: 20px;
+                background-color: green;
+                color: white;
+                line-height:0;
+            }
         }
+
         .btn {
             height: 50px;
 
