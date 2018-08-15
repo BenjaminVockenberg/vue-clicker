4047f43295c2965105531a7fc2eba310ac9d5acb
benjamin.vockenberg <benjamin.vockenberg@gmail.com>
2018-08-15 09:54:47 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: sans-serif;  
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
